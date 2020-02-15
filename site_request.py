3b397c04c8ee27a280dc41c3b5c23fc878eaf8ff
Christopher Tiller <christiller93@protonmail.com>
2020-02-15 10:54:51 -0500

    Create readme file. Update script to find via id.

--- site_request.py
+++ site_request.py
@@ -1,8 +1,12 @@
 import requests
+from bs4 import BeautifulSoup
 import pprint
 
 URL = 'https://www.monster.com/jobs/search/?q=Python-Developer&where=Tampa__2C-FL'
 page = requests.get(URL)
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(page.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='ResultsContainer')
+print(results.prettify())
+#pp = pprint.PrettyPrinter(indent=4)
+#pp.pprint(page.content)
