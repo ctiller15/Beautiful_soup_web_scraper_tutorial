8e8dce198b3a5a10edc206011a2613a8b87c173e
Christopher Tiller <christiller93@protonmail.com>
2020-02-15 22:09:06 -0500

    finish tutorial

--- site_request.py
+++ site_request.py
@@ -7,6 +7,30 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find(id='ResultsContainer')
-print(results.prettify())
+#print(results.prettify())
+
+job_elems = results.find_all('section', class_='card-content')
+python_jobs = results.find_all('h2', string=lambda text: 'python' in text.lower())
+
+print(len(python_jobs))
+
+for p_job in python_jobs:
+    link = p_job.find('a')['href']
+    print(p_job.text.strip())
+    print(f"Apply here: {link}\n")
+
+'''for job_elem in python_jobs:
+    # Each job_elem is a new BeautifulSoup object
+    # You can use the same methods on it as you did before.
+    title_elem = job_elem.find('h2', class_='title')
+    company_elem = job_elem.find('div', class_='company')
+    location_elem = job_elem.find('div', class_='location')
+    if None in (title_elem, company_elem, location_elem):
+        continue
+    print(title_elem.text.strip())
+    print(company_elem.text.strip())
+    print(location_elem.text.strip())
+    print()
+'''
 #pp = pprint.PrettyPrinter(indent=4)
 #pp.pprint(page.content)
